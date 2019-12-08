d286e4ba2b3463de78f5ccb9cb45980ed69673f1
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-08 15:24:07 +0000

    Fast push

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ device=$1
 timedatectl set-ntp true
 
 # Partition the disk
-sfdisk -W always /dev/${device} << EOF > install.log
+sfdisk -W always /dev/${device} << EOF > install.log 2>&1
 label: gpt
 name=root, size=15GiB, type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
 name=swap, size=2GiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
@@ -22,11 +22,11 @@ name=home, type=933AC7E1-2EB4-4F13-B844-0E14E2AEF915
 EOF
 
 # Creating file systems
-mkfs.ext4 /dev/${device}1 >> install.log 2>&1
-mkswap /dev/${device}2 >> install.log 2>&1
+mkfs.ext4 -L "root" /dev/${device}1 >> install.log 2>&1
+mkswap -L "swap" /dev/${device}2 >> install.log 2>&1
 swapon /dev/${device}2 >> install.log 2>&1
-mkfs.ext4 /dev/${device}3 >> install.log 2>&1
-mkfs.ext4 /dev/${device}4 >> install.log 2>&1
+mkfs.ext4 -L "boot" /dev/${device}3 >> install.log 2>&1
+mkfs.ext4 -L "home" /dev/${device}4 >> install.log 2>&1
 
 
 # Mounting root file system
@@ -42,7 +42,20 @@ mount /dev/${device}4 /mnt/home >> install.log 2>&1
 pacstrap /mnt base linux-zen linux-firmware
 
 # Create fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -L /mnt >> /mnt/etc/fstab
 
 # Change root to /mnt
 arch-chroot /mnt
+
+# Set time zone
+ln -sf /usr/share/zoneinfo/GB /etc/localtime
+hwclock --systohc
+
+# Set location
+sed -i '/en_GB.UTF-8/s/#//' /etc/locale.ge
+#sed -i '/en_US.UTF-8/s/#//' /etc/locale.ge
+#sed -i '/es_ES.UTF-8/s/#//' /etc/locale.ge
+#sed -i '/ca_ES.UTF-8/s/#//' /etc/locale.ge
+locale-gen
+echo "LANG=$(sed '/en_GB.UTF-8/s/#//' /etc/locale.ge)" > /etc/locale.conf
+
