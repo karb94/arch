0872564573e4306b2e9706e184104dee2a5494fa
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-08 23:12:49 +0000

    Fast push

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@ fi
 device=$1
 
 # Update the system clock
+echo "Updating the system clock"
 timedatectl set-ntp true
 
 # Partition the disk
+echo "Partitioning ${device} disk"
 sfdisk -W always /dev/${device} << EOF > install.log 2>&1
 label: gpt
 name=root, size=15GiB, type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
@@ -22,6 +24,7 @@ name=home, type=933AC7E1-2EB4-4F13-B844-0E14E2AEF915
 EOF
 
 # Creating file systems
+echo "Creating file systems"
 mkfs.ext4 -L "root" /dev/${device}1 >> install.log 2>&1
 mkswap -L "swap" /dev/${device}2 >> install.log 2>&1
 swapon /dev/${device}2 >> install.log 2>&1
@@ -30,6 +33,7 @@ mkfs.ext4 -L "home" /dev/${device}4 >> install.log 2>&1
 
 
 # Mounting root file system
+echo "Mounting file systems"
 mount /dev/${device}1 /mnt >> install.log 2>&1
 # Creating mounting points on /mnt
 mkdir /mnt/boot >> install.log 2>&1
@@ -38,24 +42,34 @@ mkdir /mnt/home >> install.log 2>&1
 mount /dev/${device}3 /mnt/boot >> install.log 2>&1
 mount /dev/${device}4 /mnt/home >> install.log 2>&1
 
-# Create minimal syste in /mnt by bootstrapping
+# Create minimal system in /mnt by bootstrapping
+echo "Creating minimal system at /mnt"
 pacstrap /mnt base linux-zen linux-firmware >> install.log 2>&1
 
 # Create fstab
 genfstab -L /mnt >> /mnt/etc/fstab
 
-# Change root to /mnt
-arch-chroot /mnt
+# Create new script inside the new root
+echo "Generating new script for chroot"
+cat <<EOF > /mnt/chroot.sh
+#!/usr/bin/env bash
 
 # Set time zone
 ln -sf /usr/share/zoneinfo/GB /etc/localtime
 hwclock --systohc
 
 # Set location
-sed -i '/en_GB.UTF-8/s/#//' /etc/locale.ge
-#sed -i '/en_US.UTF-8/s/#//' /etc/locale.ge
-#sed -i '/es_ES.UTF-8/s/#//' /etc/locale.ge
-#sed -i '/ca_ES.UTF-8/s/#//' /etc/locale.ge
+sed -i '/en_GB.UTF-8/s/#//' /etc/locale.gen
+#sed -i '/en_US.UTF-8/s/#//' /etc/locale.gen
+#sed -i '/es_ES.UTF-8/s/#//' /etc/locale.gen
+#sed -i '/ca_ES.UTF-8/s/#//' /etc/locale.gen
 locale-gen
 echo "LANG=$(sed '/en_GB.UTF-8/s/#//' /etc/locale.gen)" > /etc/locale.conf
 
+exit
+EOF
+
+# Change root to /mnt
+echo "Changing root to /mnt"
+echo "Configuring system"
+arch-chroot /mnt chroot.sh
