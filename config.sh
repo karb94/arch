0420dcbebd2f69e39a1c4e5fdd4f03648be2cd09
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-11 18:13:07 +0000

    Fast push

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 cd /tmp
 sudo -u nobody curl "https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz" > aurutils.tar.gz
 sudo -u nobody tar xvzf aurutils.tar.gz
+gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
 
 chgrp nobody aurutils
 # chmod g+ws /home/build
