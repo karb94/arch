0e477a8eefb4237276c8787767b5ac6181d42a2b
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-11 21:23:31 +0000

    Fast push

--- config.sh
+++ config.sh
@@ -5,9 +5,9 @@ pacman -S --asdeps git jq expac diffstat pacutils parallel wget
 aurutils_url="https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz"
 curl $aurutils_url | sudo -u nobody tar xvz --directory /tmp/
 chmod 777 /tmp/aurutils
-HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils sudo -u nobody gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
+sudo -u nobody HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
 cd /tmp/aurutils
-HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils sudo -u nobody makepkg
+sudo -u nobody HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils makepkg
 sudo -u nobody makepkg --install
 
 # chmod g+ws /home/build
