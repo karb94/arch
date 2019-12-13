a7e1b093bf537aea32d38f17dcad998b1a85fa2b
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-13 22:17:23 +0000

    Fast push

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # read -p "Enter fullname: " username
-$username=carles
+username=carles
 
 # exit when any command fails
 set -e
@@ -20,22 +20,25 @@ pacman -S --noconfirm git man-db man-pages asp
 # Create the group "sudo"
 groupadd sudo
 # Activate superuser powers for the sudo group in /etc/sudoers
-sed -ni'/# %sudo\tALL=(ALL) ALL/s/# //' /etc/sudoers
+sed -ni '/# %sudo\tALL=(ALL) ALL/s/# //' /etc/sudoers
 # Add the main user and include it in the sudo group
 useradd $username --base-dir /home --create-home -g sudo
 # Set the password of the user
 echo "Set the new password for $username:"
 passwd $username
-su $username
+
+# Download and build aurutils in $HOME/.builds
+sudo -i -u $username << EOF
 # Create a hidden directory to store custom builds
-mkdir $HOME/.builds
+mkdir \$HOME/.builds
 
 # Install aurutils
 aurutils_url="https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz"
-curl $aurutils_url | tar xvz --directory $HOME/.builds
+curl \$aurutils_url | tar xvz --directory \$HOME/.builds
 gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
-cd $HOME/.builds/aurutils
+cd \$HOME/.builds/aurutils
 makepkg -s
+EOF
 
 # Set up a new repository for aurutils called "aur"
 cat <<EOF >> /etc/pacman.conf
@@ -43,6 +46,9 @@ cat <<EOF >> /etc/pacman.conf
 [aur]
 SigLevel = Optional TrustAll
 Server = file:///var/cache/pacman/aurpkg
+
+[herecura]
+Server = https://repo.herecura.be/$repo/$arch
 EOF
 
 # Change cache options to work well with aurutils
@@ -63,7 +69,3 @@ repo-add /var/cache/pacman/aurpkg/aur.db.tar /var/cache/pacman/aurpkg/*.pkg.tar.
 # Synchronize database with pacman
 pacman -Syu
 pacman -S aurutils
-
-cd $HOME/.builds
-asp export vim
-mkdir vim/custom
