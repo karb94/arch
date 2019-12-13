f7210f5a766ec43a851d471ec0a7d4bd8253774b
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-12-13 21:35:08 +0000

    Fast push

--- config.sh
+++ config.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# Symlink DNS configuration (for networkd network manager)
-ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-pacman -Syyu
-pacman -S --noconfirm git man-db man-pages
-pacman -S --noconfirm --asdeps jq expac diffstat pacutils parallel wget
+# read -p "Enter fullname: " username
+$username=carles
 
 # exit when any command fails
 set -e
@@ -15,27 +11,31 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-# Make aurutils package in /tmp directory
-aurutils_url="https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz"
-curl $aurutils_url | sudo -u nobody tar xvz --directory /tmp/
-sudo -u nobody HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
-cd /tmp/aurutils
-sudo -u nobody HOME=/tmp/aurutils GNUPGHOME=/tmp/aurutils makepkg
+# Symlink DNS configuration (for networkd network manager)
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
-# Create the group "sudo". You will need to give
-# superuser powers to the sudo group by using visudo
+pacman -Syyu
+pacman -S --noconfirm git man-db man-pages asp
+
+# Create the group "sudo"
 groupadd sudo
-# Create the directory for the aur database and packages
-sudo install --directory /var/cache/pacman/aurpkg --group=sudo
-# Move the .xz file to the custom aur repository that we just created
-mv aurutils*pkg.tar.xz /var/cache/pacman/aurpkg
-# pacman -U aurutils*.xz
+# Activate superuser powers for the sudo group in /etc/sudoers
+sed -ni'/# %sudo\tALL=(ALL) ALL/s/# //' /etc/sudoers
+# Add the main user and include it in the sudo group
+useradd $username --base-dir /home --create-home -g sudo
+# Set the password of the user
+echo "Set the new password for $username:"
+passwd $username
+su $username
+# Create a hidden directory to store custom builds
+mkdir $HOME/.builds
 
-# Change cache options to work well with aurutils
-sed -in '/\[options\]/a \
-    CacheDir = /var/cache/pacman/pkg\
-    CacheDir = /var/cache/pacman/custom\
-    CleanMethod = KeepCurrent' /etc/pacman.conf
+# Install aurutils
+aurutils_url="https://aur.archlinux.org/cgit/aur.git/snapshot/aurutils.tar.gz"
+curl $aurutils_url | tar xvz --directory $HOME/.builds
+gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
+cd $HOME/.builds/aurutils
+makepkg -s
 
 # Set up a new repository for aurutils called "aur"
 cat <<EOF >> /etc/pacman.conf
@@ -45,9 +45,25 @@ SigLevel = Optional TrustAll
 Server = file:///var/cache/pacman/aurpkg
 EOF
 
+# Change cache options to work well with aurutils
+sed -in '/\[options\]/a \
+    CacheDir = /var/cache/pacman/pkg\
+    CacheDir = /var/cache/pacman/custom\
+    CleanMethod = KeepCurrent' /etc/pacman.conf
+
+# Create the directory for the aur database and packages
+sudo install --directory /var/cache/pacman/aurpkg --group=sudo
+
+# Move the .xz file to the custom aur repository that we just created
+mv aurutils*pkg.tar.xz /var/cache/pacman/aurpkg
+
 # Create the "aur" database and add all packages in that directory to it
 repo-add /var/cache/pacman/aurpkg/aur.db.tar /var/cache/pacman/aurpkg/*.pkg.tar.xz
 
 # Synchronize database with pacman
 pacman -Syu
 pacman -S aurutils
+
+cd $HOME/.builds
+asp export vim
+mkdir vim/custom
